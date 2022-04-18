5e12ec8c0b3e36b3f1e00df842e0216fdfa1993d
William Hwang <willhwang90@gmail.com>
2022-04-18 17:18:08 -0400

    fixed the button

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ const quotes = [
     '"To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other, and to feel. That is the purpose of life."',
     '"Let everything happen to you, Beauty and Terror, just keep going, no feeling is final"',
 ];
-btn.addEventListener('onclick', function(){
-        const randomQuote = Number.parseInt(Math.random() * quotes.length + 1)
-        output.textContent =`${quotes[randomQuote]}`;
+btn.addEventListener('click', function(){
+        const randomQuote = Math.floor(Math.random() * quotes.length)
+        output.innerHTML = quotes[randomQuote];
+    
 });
+
+
