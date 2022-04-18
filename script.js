fec93d57f7d455dece05c6aa08f9030b2563ac54
William Hwang <willhwang90@gmail.com>
2022-04-18 14:32:25 -0400

    updated code for button, trying to fix

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 
-let output = document.getElementById('output');
+const output = document.getElementById('output');
 
-let quotes = [
+const quotes = [
     '"There is nothing constant except change."',
     '"A Jack of all trades is a master of none but oftentimes better than a master of one"',
     '"Youth is wasted on the young."',
     '"To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other, and to feel. That is the purpose of life."',
     '"Let everything happen to you, Beauty and Terror, just keep going, no feeling is final"',
 ];
-btn.addEventListener('click', quoteGenerator());
-
-function quoteGenerator(){
-    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-    output.innerHTML = randomQuote;
-};
-
+btn.addEventListener('onclick', function(){
+        const randomQuote = Number.parseInt(Math.random() * quotes.length + 1)
+        output.textContent =`${quotes[randomQuote]}`;
+});
