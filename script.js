146720996c48591bc0d2b0048032466c9b7daf7e
William Hwang <willhwang90@gmail.com>
2022-04-18 13:39:05 -0400

    added quotes and code to script and updated css and index.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const h2 = document.createElement("h2");
-h2.textContent = "A QUOTE I LIKE:";
-document.querySelector("body").appendChild(h2);
+let btn = document.getElementById('btn');
+
+let output = document.getElementById('output');
+
+let quotes = [
+    '"There is nothing constant except change."',
+    '"A Jack of all trades is a master of none but oftentimes better than a master of one"',
+    '"Youth is wasted on the young."',
+    '"To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other, and to feel. That is the purpose of life."',
+    '"Let everything happen to you, Beauty and Terror, just keep going, no feeling is final"',
+];
+btn.addEventListener('click', quoteGenerator());
+
+function quoteGenerator(){
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    output.innerHTML = randomQuote;
+};
+
